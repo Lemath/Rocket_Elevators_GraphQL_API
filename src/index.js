bfa4f66845c14b297543a2733b2876c68cca8b24
Lemath <56204810+Lemath@users.noreply.github.com>
2021-11-25 18:37:16 -0500

    3 resolvers working

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const resolvers = {
       return mysql.query(testQuery, 'status');
     },
     intervention: async (_, args) => {
-      const id = args.id;
+      const { id } = args;
       const psql_response = await psql.manyOrNone(`SELECT * FROM factIntervention WHERE id = ${id}`);
       const building_id = psql_response[0].building_id;
       const building = await prisma.buildings.findUnique({ where: { id: building_id } });
@@ -19,8 +19,73 @@ const resolvers = {
       const address_string = `${address.number_and_street}, ${address.city}, ${address.country}, ${address.postal_code}`;
       const start_time_and_date = String(psql_response[0].intervention_start_date_time);
       const end_time_and_date = String(psql_response[0].intervention_end_date_time);
-      return {id: id, start_time_and_date: start_time_and_date, end_time_and_date: end_time_and_date, address: address_string};
-    } 
+      return {
+        id: id, 
+        start_time_and_date: start_time_and_date, 
+        end_time_and_date: end_time_and_date, 
+        address: address_string
+      };
+    }, 
+    building: async (_, args) => {
+      const { id } = args;
+      const building = await prisma.buildings.findUnique({ where: { id: Number(id) } });
+      const customer = await prisma.customers.findUnique({ select: {id: true, company_name: true, company_email: true, company_contact: true}, where: { id: building.customer_id } });
+      customer.id = Number(customer.id);
+      const interventions = await psql.manyOrNone(`SELECT * FROM factIntervention WHERE building_id = ${id}`);
+      const address = await prisma.addresses.findUnique({ where:{ id: building.address_id } });
+      const address_string = `${address.number_and_street}, ${address.city}, ${address.country}, ${address.postal_code}`;
+      const intervention_list = [];
+      interventions.forEach( (intervention) => {
+        intervention_list.push({
+          id: intervention.id,
+          start_time_and_date: String(intervention.intervention_start_date_time),
+          end_time_and_date: String(intervention.intervention_end_date_time),
+          address: address_string
+        });
+      });
+      return {id: Number(id), customer: customer, interventions: intervention_list};
+    },
+    employee: async (_, args) => {
+      const { id } = args;
+      const interventions = await psql.manyOrNone(`SELECT * FROM factIntervention WHERE employee_id = ${id}`);
+      const intervention_list = [];
+      const building_id_list = [];
+      interventions.forEach( (intervention) => {
+        intervention_list.push({
+          id: intervention.id,
+          start_time_and_date: String(intervention.intervention_start_date_time),
+          end_time_and_date: String(intervention.intervention_end_date_time),
+          address: `address_string`
+        });
+        console.log(`building id : ${intervention.building_id}`)
+        if (building_id_list.indexOf(intervention.building_id) === -1) {
+          building_id_list.push(intervention.building_id);
+        }
+      });
+      console.log(building_id_list);
+      console.log(`building ids : ` + building_id_list);
+      const building_list = await Promise.all(building_id_list.map( async (id) => {
+        const building = await prisma.buildings.findUnique({ where: { id: Number(id) } });
+        const building_details = await prisma.building_details.findMany({ where: { building_id: id } });
+        console.log(building_details);
+        return {
+          id: Number(building.id), 
+          building_details: {
+            number_of_floors: building_details.number_of_floors,
+            type: building_details.type,
+            department: building_details.department,
+            year_of_contruction: building_details.year_of_contruction,
+            maximum_number_of_occupants: building_details.maximum_number_of_occupants,
+          }
+        };
+      }));
+      console.log(intervention_list);
+      console.log(building_list);
+      building_list.forEach((building) => {
+
+      });
+      return {id: id, interventions: intervention_list, buildings: building_list};
+    },
   },
 };
 
