9c8feed81738b0771f8bafb5a2be9dfe82fd8713
Lemath <56204810+Lemath@users.noreply.github.com>
2021-11-24 23:03:03 -0500

    damn more clean up

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ const resolvers = {
     },
     intervention: async (_, args) => {
       const id = args.id;
-      const psql_response = await psql.manyOrNone(`SELECT * FROM factIntervention WHERE id = ${id}`)
-      const building_id = psql_response[0].building_id   
+      const psql_response = await psql.manyOrNone(`SELECT * FROM factIntervention WHERE id = ${id}`);
+      const building_id = psql_response[0].building_id;
       const building = await prisma.buildings.findUnique({ where: { id: building_id } });
       const address = await prisma.addresses.findUnique({ where:{ id: building.address_id } });
-      const address_string = `${address.number_and_street}, ${address.city}, ${address.country}, ${address.postal_code}`
+      const address_string = `${address.number_and_street}, ${address.city}, ${address.country}, ${address.postal_code}`;
       const start_time_and_date = String(psql_response[0].intervention_start_date_time);
       const end_time_and_date = String(psql_response[0].intervention_end_date_time);
       return {id: id, start_time_and_date: start_time_and_date, end_time_and_date: end_time_and_date, address: address_string};
